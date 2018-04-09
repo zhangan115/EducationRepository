5037fb22697edf1268d3af811e849d11acff8770
zhangan115 <zhangan115@126.com>
2018-04-09 23:27:44 +0800

    create project

--- EducationProject/app/src/main/java/com/xueli/application/view/user/information/UserInformationActivity.java
+++ EducationProject/app/src/main/java/com/xueli/application/view/user/information/UserInformationActivity.java
@@ -1,8 +1,12 @@
 package com.xueli.application.view.user.information;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.library.utils.GlideUtils;
 import com.xueli.application.R;
+import com.xueli.application.app.App;
 import com.xueli.application.view.BaseActivity;
 
 /**
@@ -16,6 +20,10 @@ public class UserInformationActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setLayoutAndToolbar(R.layout.user_information_activity, "个人信息");
+        ImageView ivUserPhoto = findViewById(R.id.ivUserPhoto);
+        GlideUtils.ShowImage(this, App.getInstance().getCurrentUser().getHeadImage(), ivUserPhoto, R.drawable.img_avatar);
+        TextView tvUserNickName = findViewById(R.id.tvUserNickName);
+        tvUserNickName.setText(App.getInstance().getCurrentUser().getAccountName());
 
     }
 }
