3ff221c151bc46251d80128ad338337e067c3e7a
zhangan <zhangan115@gmail.com>
2018-11-16 22:35:54 +0800

    1.5完成

--- EducationProject/app/src/main/java/com/xueli/application/view/user/about_us/AboutUsActivity.java
+++ EducationProject/app/src/main/java/com/xueli/application/view/user/about_us/AboutUsActivity.java
@@ -1,13 +1,16 @@
 package com.xueli.application.view.user.about_us;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
 import com.xueli.application.R;
+import com.xueli.application.common.ConstantStr;
 import com.xueli.application.util.APKVersionCodeUtils;
 import com.xueli.application.view.BaseActivity;
 import com.xueli.application.view.WebActivity;
+import com.xueli.application.view.web.MessageDetailActivity;
 
 import org.w3c.dom.Text;
 
@@ -28,7 +31,11 @@ public class AboutUsActivity extends BaseActivity {
         findViewById(R.id.showInfoTv).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                Intent intent = new Intent(AboutUsActivity.this, MessageDetailActivity.class);
+                intent.putExtra(ConstantStr.KEY_BUNDLE_BOOLEAN, true);
+                intent.putExtra(ConstantStr.KEY_BUNDLE_STR_1, "http://39.106.210.43/FxExam/resources/banquan.html");
+                intent.putExtra(ConstantStr.KEY_BUNDLE_STR, "版权声明");
+                startActivity(intent);
             }
         });
     }
